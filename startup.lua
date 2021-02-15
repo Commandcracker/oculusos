eb22b05b0d4e2a2d5ee0a988db83e19e958639ba
Commandcracker <fricke-lukas@web.de>
2021-02-15 11:55:26 +0100

    fixed startup

--- startup.lua
+++ startup.lua
@@ -1,6 +1,6 @@
 for _, sFile in ipairs(fs.list("/boot")) do
     if string.sub(sFile, 1, 1) ~= "." then
-        local sPath = "/boot" .. sFile
+        local sPath = "/boot/" .. sFile
         if not fs.isDir(sPath) then
             shell.run(sPath)
         end
