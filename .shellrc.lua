c14d9a48c602bd220a2772e57835e19e02a24a6c
Commandcracker <4083674-Commandcracker@users.noreply.gitlab.com>
2021-07-27 18:35:09 +0200

    fixed .shellrc.lua

--- .shellrc.lua
+++ .shellrc.lua
@@ -1,12 +1,12 @@
 --[[
 
-	\d   the date in "Weekday Month Date" format (e.g., "Tue May 26")
-	\h   The hostname.
-	\t   the current time in 24-hour HH:MM:SS format
-	\T   the current time in 12-hour HH:MM:SS format
-	\@   the current time in 12-hour am/pm format
-	\A   the current time in 24-hour HH:MM format
-	\w   The current working directory.
+	\\d   the date in "Weekday Month Date" format (e.g., "Tue May 26")
+	\\h   The hostname.
+	\\t   the current time in 24-hour HH:MM:SS format
+	\\T   the current time in 12-hour HH:MM:SS format
+	\\@   the current time in 12-hour am/pm format
+	\\A   the current time in 24-hour HH:MM format
+	\\w   The current working directory.
 
 	\n   A newline.
 
@@ -32,4 +32,4 @@
 		f black
 ]]
 
-_G.PS1="&b(&e\h&b)-[&0\w&b]\n&e# "
+_G.PS1="&b(&e\\h&b)-[&0\\w&b]\n&e# "
--- src/bin/df.lua
+++ src/bin/df.lua
@@ -1,5 +1,3 @@
-print("Filesystem FreeSpace")
-
 local function size(nSpace)
 	if nSpace >= 1000 * 1000 then
 		return (math.floor( nSpace / (100 * 1000) ) / 10) .. "MB"
@@ -10,18 +8,38 @@ local function size(nSpace)
 	end
 end
 
-local function display(path)
-    f = fs.getFreeSpace(path)
-    print(path.." "..size(f))
+data = {
+	{"Filesystem", "FreeSpace"}
+}
+
+local function add(path)
+	table.insert(data, {path, size(fs.getFreeSpace(path))})
 end
 
-display("/")
+add("/")
 
 for _, side in ipairs(peripheral.getNames()) do
     if peripheral.getType(side) == "drive" then
 		mount = disk.getMountPath(side)
 		if mount then
-        	display('/'..mount)
+        	add('/'..mount)
 		end
 	end
+end
+
+Filesystem_space = 0
+for k, v in pairs(data) do
+	if v[1]:len() > Filesystem_space then
+		Filesystem_space = v[1]:len()
+	end
+end
+
+for k, v in pairs(data) do
+	for i = Filesystem_space - v[1]:len(), 0, -1 do
+		v[1] = v[1].." "
+	end
+end
+
+for k, v in pairs(data) do
+	print(v[1]..v[2])
 end
