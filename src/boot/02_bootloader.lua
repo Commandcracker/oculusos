c418410ff188da05e5f407ded9b7334314cb47aa
Commandcracker <4083674-Commandcracker@users.noreply.gitlab.com>
2021-07-30 23:45:17 +0200

    fixed load pack

--- src/boot/02_bootloader.lua
+++ src/boot/02_bootloader.lua
@@ -184,8 +184,14 @@ local function register_programs()
     end
 
     -- load pack
-    if fs.exists("/etc/pack/packages/pack/pack/lib/pack") then
-        dofile("/etc/pack/packages/pack/pack/lib/pack").loadPackages(shell)
+    local pack_path = "/etc/pack/packages/pack/"
+    local pack_src_path = pack_path.."pack-src/lib/pack"
+    local pack_min_path = pack_path.."pack/lib/pack"
+    
+    if fs.exists(pack_src_path) then
+        dofile(pack_src_path).loadPackages(shell)
+    elseif fs.exists(pack_min_path) then
+        dofile(pack_min_path).loadPackages(shell)
     end
 
     sleep(1)
